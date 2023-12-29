279184fee76bcb48d2364303e328c4ad27eb5106
Enes Akkuş <116295097+enesvct@users.noreply.github.com>
2023-12-29 16:01:27 +0300

    online işlem route engellendi

--- EkoFaktoring.UI/Controllers/OnlineIslemlerController.cs
+++ EkoFaktoring.UI/Controllers/OnlineIslemlerController.cs
@@ -4,11 +4,12 @@ namespace EkoFaktoring.UI.Controllers
 {
     public class OnlineIslemlerController : CustomBaseController
     {
+        [NonAction]
         public IActionResult OnlineIslem()
         {
             return View();
         }
-
+        [NonAction]
         public IActionResult BasvuruTakip()
         {
             return View();
